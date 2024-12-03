1d50990e051c0116af2c57ea3370564dac3b960c
Unmatched TaiNguyen <tainguyen.ntt.97@gmail.com>
2024-12-03 09:45:44 +0700

    update

--- bin/pi.bat
+++ bin/pi.bat
@@ -2,14 +2,8 @@
 SET batchDir=%~dp0
 REM Check if a parameter was provided
 IF "%1"=="" (
-    echo Please provide the command (ilib, ireact, or ireactlib)
+    echo Please provide the command (lib, react, or reactlib)
     @REM exit /b
-)
-
-REM Check if the provided command is valid
-IF NOT "%1"=="ilib" IF NOT "%1"=="ireact" IF NOT "%1"=="ireactlib" (
-    echo Invalid command! Please use one of the following: ilib, ireact, ireactlib
-    exit /b
 )
 
 REM Call the Node.js script located in ../src/ directory with the provided parameter
